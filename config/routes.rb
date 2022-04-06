a58680342b1c3ae9b7a9ca6a3ea3bd6a6d7ffe15
Jake Pople <jakepople@hotmail.co.uk>
2022-04-06 19:13:09 +0100

    pages set up

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/composer', to: 'pages#composer', as: 'composer'
+  get '/performer', to: 'pages#performer', as: 'performer'
+  get '/arranger', to: 'pages#arranger', as: 'arranger'
+  get '/md', to: 'pages#md', as: 'md'
 end
