3c57ee160858b4b27071ff0f7e9650578c059057
Jake Pople <jakepople@hotmail.co.uk>
2022-04-12 15:25:10 +0100

    seeds updated with type

--- db/migrate/20220408103557_create_projects.rb
+++ db/migrate/20220408103557_create_projects.rb
@@ -2,6 +2,7 @@ class CreateProjects < ActiveRecord::Migration[6.1]
   def change
     create_table :projects do |t|
       t.string :title
+      t.string :type
       t.string :description
       t.string :image_path
       t.string :iframe_url
