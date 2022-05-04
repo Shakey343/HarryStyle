e57afc59dfaae976b04513e4507a0ae9468d3283
Jake Pople <jakepople@hotmail.co.uk>
2022-05-04 11:14:53 +0100

    meta tags added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
   # before_action :authenticate_user!
+  def default_url_options
+    { host: ENV["https://www.harrystyle.co.uk"] || "localhost:3000" }
+  end
 end
